5a15fadab8b124c0c915b71de5321394d221fd09
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-06-26 18:40:57 +0700

    25/06/2023

--- admin/account/loginProcess.php
+++ admin/account/loginProcess.php
@@ -25,6 +25,6 @@ if($count_account == 0) {
     $_SESSION['id'] = $id;
     $_SESSION['email_admin'] = $email;
     // Sang trang danh sách
-    header("Location: ../start/index.php");
+    header("Location: ../order_manage/index.php");
 }
 ?>
