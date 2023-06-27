11b4dc40f8c8fcfea63dd6598cc0a374f71d7b9d
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-06-27 17:25:15 +0700

    Update create.php

--- admin/clothes_manage/create.php
+++ admin/clothes_manage/create.php
@@ -13,7 +13,37 @@
         body {
             background-color: #F5F4F8;
         }
+        #image {
+            margin-top:30px;
+            width:150px;
+            height: 150px;
+            border:1px solid #b1bac0;
+            object-fit: cover;
+            position:absolute;
+        }
+
+        #imageFile {
+            position:absolute;
+            margin-top:180px;
+            margin-left:30px;
+            max-width: 109px;
+            height:33px;
+        }
     </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script>
+        function chooseFile(fileInput) {
+            if (fileInput.files && fileInput.files[0]) {
+                let reader = new FileReader();
+
+                reader.onload = function(e) {
+                    $('#image').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(fileInput.files[0]);
+            }
+        }
+    </script>
     <title> Add a clothe </title>
 </head>
 <body>
@@ -36,7 +66,7 @@ include_once "../connect/close.php";
        Name: <input style="margin-bottom: 18px" type="text" name="clothe_name"><br>
        Material: <input style="margin-bottom: 18px" type="text" name="material"><br>
        Size: <input style="margin-bottom: 18px; width: 300px" name="size"><br>
-       Color: <input style="margin-bottom: 18px" type="text" name="color"><br>
+       Color:  <input style="margin-bottom: 18px" type="text" name="color"><br>
        Description: <input style="margin-bottom: 18px" type="text" name="description"><br>
        Category ID: <select style="margin-bottom: 18px" name="category_id">
            <option> - Choose - </option>
@@ -50,9 +80,8 @@ include_once "../connect/close.php";
            <?php
                 }
            ?>
-       </select>
-       <br>
-       Producer ID: <select style="margin-bottom: 18px" name="producer_id">
+       </select><br>
+        Producer ID: <select style="margin-bottom: 18px" name="producer_id">
            <option> - Choose - </option>
            <?php
                foreach ($producers as $producer){
@@ -66,10 +95,14 @@ include_once "../connect/close.php";
        </select><br>
        Quantity: <input style="margin-bottom: 18px" type="text" name="quantity"><br>
        Price: <input style="margin-bottom: 18px" type="text" name="price"><br>
-       Image: <input type="file" name="image"><br>
+       Image: <input type="file" name="image" id="imageFile" onchange="chooseFile(this)"
+                     accept="image/gif, image/png, image/jpeg">
+                <img src="" alt="" id="image" width="200px">
+        <br><br><br><br><br><br><br>
        <button class="btn add btn-primary" type="submit"> Add </button>
     </form>
 </div>
+    <br>
 </section>
 
 </body>
