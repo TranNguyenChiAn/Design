1495d9b7f0b816d6699ed1d023868facfad55378
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-06-28 23:32:41 +0700

    Update index.php

--- customer/pages/index.php
+++ customer/pages/index.php
@@ -5,6 +5,15 @@
         body{
             background-color: #F5F4F8;
         }
+        .search_clothe {
+            width: 350px;
+            height: 35px;
+            border-radius: 20px;
+            border: 1px solid #9f9b9b;
+            right: 20px;
+            padding-left: 20px;
+            margin-left: 600px;
+        }
     </style>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta charset="UTF-8">
@@ -12,6 +21,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
           crossorigin="anonymous">
@@ -20,7 +31,7 @@
 <body>
 <!-------------------- HEADER -------------------->
 <?php
-    include_once '../layout/header.php';
+include_once '../layout/header.php';
 //Mở kết nối
 include_once '../connect/open.php';
 //Khai báo biến search
@@ -33,6 +44,7 @@ if(isset($_GET['search'])) {
 <!-------------------- END HEADER -------------------->
 
 </section>
+<div>
     <!-------------------- MAIN -------------------->
     <div class="headline">
         <!--SEARCH-->
@@ -46,10 +58,7 @@ if(isset($_GET['search'])) {
             //Khai báo số bản ghi 1 trang
             $recordOnePage = 12;
             //Query để lấy số bản ghi
-            $sqlCountRecord = "SELECT COUNT(*) AS count_record FROM clothes
-                                WHERE name LIKE '%$search%'
-                                OR color LIKE '%$search%'
-                                OR size LIKE '%$search%'";
+            $sqlCountRecord = "SELECT COUNT(*) AS count_record FROM clothes";
             //Chạy query lấy số bản ghi
             $countRecords = mysqli_query($connect, $sqlCountRecord);
             //foreach để lấy số bản ghi
@@ -65,18 +74,19 @@ if(isset($_GET['search'])) {
             }
             //Tính bản ghi bắt đầu của trang
             $start = ($page - 1) * $recordOnePage;
-            $sql = "SELECT * FROM clothes
+            $sql = "SELECT clothes.* FROM clothes
                     INNER JOIN categories ON clothes.category_id = categories.id
-                          WHERE clothes.name LIKE '%$search%'
-                          OR clothes.color LIKE '%$search%'
-                          OR clothes.size LIKE '%$search%'
-                          OR categories.name LIKE '%$search%'
+                    WHERE clothes.name LIKE '%$search%'
+                        OR clothes.color LIKE '%$search%'
+                        OR clothes.size LIKE '%$search%'
+                        OR categories.name LIKE '%$search%'
                     LIMIT $start, $recordOnePage";
             $clothes = mysqli_query($connect, $sql);
-                foreach ($clothes as $clothe) {
-            ?>
+            include_once '../connect/close.php';
+            foreach ($clothes as $clothe){
+                ?>
                 <div class="col-3" style="">
-                    <a  href="product_detail.php?id=<?= $clothe['id'] ?>">
+                    <a href="product_detail.php?id=<?= $clothe['id'] ?>">
                         <img style="width:278px; height: 278px; object-fit: cover" src="../../image/<?= $clothe['image'] ?>" alt="BEST SELLER" >
                     </a>
                     <a href="../carts/add to cart.php?id=<?= $clothe['id']; ?>">
@@ -87,22 +97,22 @@ if(isset($_GET['search'])) {
                     <hr style="width:200px;margin:6px 0">
                     <span class="price">$<?= $clothe['price'] ?></span>
                 </div>
-                    <?php
-                }
-                ?>
+                <?php
+            }
+            ?>
         </div>
     </div>
     <div align="center">
         <!--        form để hiển thị số trang-->
         <?php
-            for($i = 1; $i <= $countPage; $i++){
-                ?>
-                <a style="color:black; text-decoration: none; font-weight: bold; font-family: '#9Slide03 BoosterNextFYBlack'" class="page_number" href="?page=<?= $i ?>">
-                    &nbsp &nbsp  <?= $i ?>
-                </a>
+        for($i = 1; $i <= $countPage; $i++){
+            ?>
+            <a style="color: black;text-decoration: none; font-weight: bold; font-family: '#9Slide03 BoosterNextFYBlack'" class="page_number" href="?page=<?= $i ?>">
+                &nbsp &nbsp <?= $i ?>  &nbsp;
+            </a>
 
-                <?php
-            }
+            <?php
+        }
         ?>
     </div>
 </div>
