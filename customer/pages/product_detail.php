658c8c9eb1752c26d7f5f6a6d2b85b1da3ffbc83
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-06-28 23:32:38 +0700

    Update product_detail.php

--- customer/pages/product_detail.php
+++ customer/pages/product_detail.php
@@ -6,6 +6,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            background-color: white;
+        }
+    </style>
     <title> Product's Detail</title>
 </head>
 <body>
