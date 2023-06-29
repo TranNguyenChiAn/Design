1853fad4d5ab22bf754d1e5f4ce27ac4acce061a
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-06-29 17:03:18 +0700

    Update cancel.php

--- customer/profile/cancel.php
+++ customer/profile/cancel.php
@@ -1,7 +1,17 @@
 <?php
 $id = $_GET['id'];
+header("location.reload()");
 include_once '../connect/open.php';
-$sql = "UPDATE orders SET status = 3 WHERE id = '$id'";
-mysqli_query($connect, $sql);
+$sql = "SELECT * FROM orders WHERE id = '$id'";
+$orders = mysqli_query($connect, $sql);
+foreach($orders as $order) {
+    if($order['status'] == 0) {
+        header("location.reload()");
+        $sql = "UPDATE orders SET status = 3 WHERE id = '$id'";
+        mysqli_query($connect, $sql);
+    }else {
+        header("location.reload()");
+    }
+}
 header("Location: index.php");
 ?>
