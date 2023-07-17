72eea31d35308b77c88f99c85fb3c6fc8374ec62
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-07-17 22:45:24 +0700

    Create momo.php

---
+++ customer/payment/online/momo.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Momo payment</title>
+</head>
+<body>
+<domain>/pay/store/<storeSlug>?a=<amount>&b=<billId>&s=<signature>
+
+</body>
+</html>
+<?php
+
+?>
