90a935e65a2ef521007ca0ddc3128d19dc173461
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-07-17 22:45:08 +0700

    Update new.php

--- customer/pages/new.php
+++ customer/pages/new.php
@@ -21,6 +21,12 @@
             margin: -24px 0 0 -50px;
             rotate: 30deg;
         }
+        .sold_out {
+            width: 90px;
+            height: auto;
+            position: absolute;
+            margin: 90px 0 0 90px;
+        }
     </style>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <meta charset="UTF-8">
@@ -63,79 +69,89 @@ include_once '../layout/header.php';
         <p class="trend"> new arrivals</p>
         <div class="maincontent">
             <?php
-            //Khai báo số bản ghi 1 trang
-            $recordOnePage = 8;
-            //Query để lấy số bản ghi
-            $sqlCountRecord = "SELECT COUNT(*) AS count_record FROM clothes";
-            //Chạy query lấy số bản ghi
-            $countRecords = mysqli_query($connect, $sqlCountRecord);
-            //foreach để lấy số bản ghi
-            foreach ($countRecords as $countRecord) {
-                $records = $countRecord['count_record'];
-            }
-            //Tính số trang
-            $countPage = ceil($records / $recordOnePage);
-            //Lấy trang hiện tại (nếu không tồn tại page hiện tại thì page hiện tại = 1)
-            $page = 1;
-            if (isset($_GET['page'])) {
-                $page = $_GET['page'];
-            }
-            //Tính bản ghi bắt đầu của trang
-            $start = ($page - 1) * $recordOnePage;
-            $sql = "SELECT clothes.* FROM clothes
-                    INNER JOIN categories ON clothes.category_id = categories.id
-                    WHERE clothes.name LIKE '%$search%'
-                        OR clothes.color LIKE '%$search%'
-                        OR clothes.size LIKE '%$search%'
-                        OR categories.name LIKE '%$search%'
-                    ORDER BY clothes.id DESC
-                    LIMIT $start, $recordOnePage";
-            $clothes = mysqli_query($connect, $sql);
-            include_once '../connect/close.php';
-            foreach ($clothes as $clothe){
-                ?>
+                //Khai báo số bản ghi 1 trang
+                $recordOnePage = 8;
+                //Query để lấy số bản ghi
+                $sqlCountRecord = "SELECT COUNT(*) AS count_record FROM clothes";
+                //Chạy query lấy số bản ghi
+                $countRecords = mysqli_query($connect, $sqlCountRecord);
+                //foreach để lấy số bản ghi
+                foreach ($countRecords as $countRecord) {
+                    $records = $countRecord['count_record'];
+                }
+                //Tính số trang
+                $countPage = ceil($records / $recordOnePage);
+                //Lấy trang hiện tại (nếu không tồn tại page hiện tại thì page hiện tại = 1)
+                $page = 1;
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                }
+                //Tính bản ghi bắt đầu của trang
+                $start = ($page - 1) * $recordOnePage;
+                $sql = "SELECT clothes.* FROM clothes
+                        INNER JOIN categories ON clothes.category_id = categories.id
+                        WHERE clothes.name LIKE '%$search%'
+                            OR clothes.color LIKE '%$search%'
+                            OR clothes.size LIKE '%$search%'
+                            OR categories.name LIKE '%$search%'
+                        ORDER BY clothes.id DESC
+                        LIMIT $start, $recordOnePage";
+                $clothes = mysqli_query($connect, $sql);
+                include_once '../connect/close.php';
+                foreach ($clothes as $clothe){
+            ?>
                 <div class="col-3" style="">
-                    <a  href="product_detail.php?id=<?= $clothe['id'] ?>">
+                    <a href="product_detail.php?id=<?= $clothe['id'] ?>">
                         <img style="width:278px; height: 278px; object-fit: cover" src="../../image/<?= $clothe['image'] ?>" alt="NEW" >
                         <?php
-                        if($clothe['quantity'] < 9){
-                            ?>
+                            if($clothe['quantity'] < 9 and $clothe['quantity'] > 0){
+                        ?>
                             <img class="out_of_stock" src="../../image/out_of_stock.png">
-                            <?php
-                        }
+                        <?php
+                            }
                         ?>
                     </a>
-                    <a href="../carts/add to cart.php?id=<?= $clothe['id']; ?>">
-                        <img class="cart_symbol" src="../../image/shopping-cart.png">
-                    </a>
+                    <?php
+                        if($clothe['quantity'] > 0){
+                    ?>
+                        <a href="../carts/add to cart.php?id=<?= $clothe['id']; ?>">
+                            <img class="cart_symbol" src="../../image/shopping-cart.png">
+                        </a>
+                    <?php
+                        }else {
+                    ?>
+                        <img class="sold_out" src="../../image/sold_out.png">
+                    <?php
+                        }
+                    ?>
                     <br>
                     <span class="product_name"><?= $clothe['name'] ?></span>
                     <hr style="width:200px;margin:6px 0">
                     <span class="price">$<?= $clothe['price'] ?></span>
                 </div>
-                <?php
-            }
+            <?php
+                }
             ?>
         </div>
     </div>
     <div align="center">
         <!--        form để hiển thị số trang-->
         <?php
-        for($i = 1; $i <= $countPage; $i++){
-            ?>
+            for($i = 1; $i <= $countPage; $i++){
+        ?>
             <a style="color: black;text-decoration: none; font-weight: bold; font-family: '#9Slide03 BoosterNextFYBlack'" class="page_number" href="?page=<?= $i ?>">
                 &nbsp &nbsp <?= $i ?>  &nbsp;
             </a>
 
-            <?php
-        }
+        <?php
+            }
         ?>
     </div>
 </div>
 
 <!-------------------- FOOTER -------------------->
 <?php
-include_once '../layout/footer.php';
+    include_once '../layout/footer.php';
 ?>
 <!-------------------- END FOOTER -------------------->
 
