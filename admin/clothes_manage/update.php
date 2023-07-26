3df9aad903c09ed38b3dcac4f66a5e33cf8255b4
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-07-26 15:18:14 +0700

    Update update.php

--- admin/clothes_manage/update.php
+++ admin/clothes_manage/update.php
@@ -1,36 +1,36 @@
 <?php
-//Lấy dữ liệu
-$id = $_POST['id'];
-$name = $_POST['clothe_name'];
-$material = $_POST['material'];
-$size = $_POST['size'];
-$color = $_POST['color'];
-$description = $_POST['description'];
-$category_id = $_POST['category_id'];
-$producer_id = $_POST['producer_id'];
-$quantity = $_POST['quantity'];
-$price = $_POST['price'];
-if(!empty($_FILES['image']['name'])){
-    $image = $_FILES['image']['name'];
-}
+    //Lấy dữ liệu
+    $id = $_POST['id'];
+    $name = $_POST['clothe_name'];
+    $material = $_POST['material'];
+    $size = $_POST['size'];
+    $color = $_POST['color'];
+    $description = $_POST['description'];
+    $category_id = $_POST['category_id'];
+    $producer_id = $_POST['producer_id'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+    if(!empty($_FILES['image']['name'])){
+        $image = $_FILES['image']['name'];
+    }
 
-//Mở kết nối
-include_once '../connect/open.php';
-//Query để update dữ liệu
-$sql = "UPDATE clothes SET name = '$name', material = '$material', size = '$size', color = '$color', description = '$description', category_id = '$category_id',
-producer_id = '$producer_id', quantity = '$quantity', price = '$price', image = '$image' WHERE id = '$id'";
-//Chạy query
-mysqli_query($connect, $sql);
+    //Mở kết nối
+    include_once '../connect/open.php';
+    //Query để update dữ liệu
+    $sql = "UPDATE clothes SET name = '$name', material = '$material', size = '$size', color = '$color', description = '$description', category_id = '$category_id',
+    producer_id = '$producer_id', quantity = '$quantity', price = '$price', image = '$image' WHERE id = '$id'";
+    //Chạy query
+    mysqli_query($connect, $sql);
 
-//Kiểm tra ảnh đã tồn tạo trong folder chưa
-if(!file_exists('../../../image/' . $image)) {
-    //Lay path cua anh
-    $path = $_FILES['image']['tmp_name'];
-    //Luu anh
-    move_uploaded_file($path, "../../image/" . $image);
-    echo $path;
-}
-//Quay về trang danh sách
-header('Location:../clothes_manage/index.php');
+    //Kiểm tra ảnh đã tồn tạo trong folder chưa
+    if(!file_exists('../../../image/' . $image)) {
+        //Lay path cua anh
+        $path = $_FILES['image']['tmp_name'];
+        //Luu anh
+        move_uploaded_file($path, "../../image/" . $image);
+        echo $path;
+    }
+    //Quay về trang danh sách
+    header('Location:../clothes_manage/index.php');
 
 ?>
