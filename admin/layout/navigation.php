e2b5d88b118338b4e5584463f2ddc3009197edff
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-07-30 15:53:34 +0700

    Update navigation.php

--- admin/layout/navigation.php
+++ admin/layout/navigation.php
@@ -29,6 +29,7 @@
             flex-direction: column;
             border-bottom-right-radius: 10px;
             border-top-right-radius: 10px;
+            font-size: 16px;
         }
 
         .choice{
@@ -36,7 +37,7 @@
             height: 50px;
             color: #6868de;
             text-decoration: none;
-            padding-left: 28%;
+            padding-left: 24%;
             padding-top: 10px;
         }
 
@@ -59,27 +60,53 @@
     <div class="navbar">
         <ul class="navbar_menu">
             <li style="margin-left: 45px">
-                <img width="100px" src="../../image/celement cat.png">
+                <?php
+                    include_once "../connect/open.php";
+                    $email = $_SESSION['email_admin'];
+                    $sql = "SELECT name FROM admins WHERE email = '$email'";
+                    $admins = mysqli_query($connect, $sql);
+                ?>
+                <img width="30px" src="../../image/celement cat.png">
+                <?php
+                    foreach ($admins as $admin){
+                ?>
+                <span>
+                    &nbsp <?= $admin['name'] ?>
+                </span>
+                <?php
+                    }
+                ?>
             </li>
             <li class="choice">
-                <a class="link_in_menu" href="../homepage/index.php"> Overview </a>
+                <img style="width:16px; margin-top: -3px" src="../../image/statistic.png">
+                <a class="link_in_menu" href="../homepage/index.php"> Statistics </a>
+            </li>
+            <li  class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/package.png">
+                <a class="link_in_menu" href="../clothes_manage/index.php"> Product </a>
             </li>
             <li class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/boxes.png">
+                <a class="link_in_menu" href="../category_manager/index.php"> Warehouse </a>
+            </li>
+            <li class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/menu.png">
                 <a class="link_in_menu" href="../category_manager/index.php"> Category </a>
             </li>
             <li  class="choice">
-                <a class="link_in_menu" href="../clothes_manage/index.php"> Clothes </a>
-            </li>
-            <li  class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/machice.png">
                 <a class="link_in_menu" href="../producer_manage/index.php"> Producer </a>
             </li>
             <li  class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/customer.png">
                 <a class="link_in_menu" href="../customer_manage/index.php"> Customer </a>
             </li>
             <li  class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/shopping-cart (1).png">
                 <a class="link_in_menu" href="../order_manage/index.php"> Order </a>
             </li>
             <li  class="choice">
+                <img style="width:16px; margin-top: -3px" src="../../image/log-out.png">
                 <a class="link_in_menu" href="../account/logout_admin.php"> Logout </a>
             </li>
         </ul>
