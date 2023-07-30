3128a9dc68f5a9fd8dbb158d76ce5bcc44d135dd
TranNguyenChiAn <117379845+TranNguyenChiAn@users.noreply.github.com>
2023-07-30 15:53:48 +0700

    Create sales_yearly.php

---
+++ admin/homepage/statistic/sales_yearly.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+    <style>
+        body {
+            background-color: white;
+        }
+
+        #chart-container {
+            width:100%;
+            max-width:600px;
+        }
+    </style>
+    <title> Homepage </title>
+</head>
+<body>
+<?php
+//mo ket noi
+include_once "../../connect/open.php";
+//Query để lấy dữ liệu từ bảng classes trên db về
+$sql = "SELECT sum(order_details.quantity) as quantity, YEAR(orders.date_buy) as date_buy
+        FROM order_details
+        INNER JOIN orders ON order_details.order_id = orders.id
+        WHERE orders.status = 2
+        group by YEAR(orders.date_buy)";
+//Chay query
+$result = mysqli_query($connect, $sql);
+
+foreach ($result as $data) {
+    $amount[] = $data['quantity'];
+    $date[] = $data['date_buy'];
+    //bỏ cảnh báo
+    error_reporting(E_ALL ^ E_WARNING);
+}
+?>
+<section style="margin: 0 0 0 90px" class="main_content">
+    <div id="chart-container">
+        <h4 style="margin: 30px 0 18px 40%;"> SOLD PRODUCTS </h4>
+        <canvas id="myChart" style="display: block;width:100%;max-width:600px;"></canvas>
+    </div>
+    <script>
+        const monthArray = <?php echo json_encode($date)?>;
+        console.log(monthArray);
+
+        const data = {
+            labels: <?php echo json_encode($date)?>,
+            datasets: [{
+                label: 'Number of sold products',
+                fill: false,
+                lineTension: 0,
+                backgroundColor: ['rgb(116,116,227)'],
+                borderColor: ['rgba(0,0,255,0.1)'],
+                data: <?php echo json_encode($amount)?>,
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data,
+            options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                    labels: {
+                        fontColor: '#000000'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Sales',
+                    fontSize: 25
+                }
+            }
+        };
+
+        //render chart
+        const myChart = new Chart(
+            document.getElementById('myChart'),
+            config
+        );
+    </script>
+</section>
+
+</body>
+</html>
